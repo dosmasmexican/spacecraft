152f2803ca634e5c14568c1fec5ebffeabb39634
dosmasmexican <145898871+dosmasmexican@users.noreply.github.com>
2023-10-31 11:53:05 +0900

    유체저항 입력

--- sketch.js
+++ sketch.js
@@ -1,7 +1,40 @@
+// Five moving bodies
+let spaceCraft = [];
+
+// Liquid
+let liquid;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 240);
+  reset();
+  // Create liquid object
+  liquid = new Liquid(0, height / 2, width, height / 2, 0.1);
 }
 
 function draw() {
-  background(220);
+  background(255);
+
+  // Draw liquid
+  liquid.show();
+
+      let dragForce = liquid.calculateDrag(spaceCraft);
+      spaceCraft.applyForce(dragForce);
+    
+
+    
+    spaceCraft.update();
+    spaceCraft.show();
+    spaceCraft.checkEdges();
+  }
+}
+
+function mousePressed() {
+  reset();
+}
+
+// Restart all the Mover objects randomly
+function reset() {
+  for (let i = 0; i < 9; i++) {
+    movers[i] = new Mover(40 + i * 70, 0, random(0.5, 3));
+  }
 }
